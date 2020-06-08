d132a148e334033c15802ad1747c55340d1edec1
MxFxM <mxfxmmueller@gmail.com>
2020-06-08 18:54:09 +0200

    added sliders to modify the output in realtime

--- pendulumShapes/pendulum1.py
+++ pendulumShapes/pendulum1.py
@@ -1,25 +1,66 @@
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 import math
 
-time = [n*0.01 for n in range(2000)]
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.5)
 
-amplitudeX = 1 # amplitude of oscillation
-frequencyX = 1.5 # frequency in Hz (undamped)
-phaseX = 2 # phase offset in radians
-dampingX = 0.01 # damping ratio
-amplitudeY = 1 # amplitude of oscillation
-frequencyY = 1 # frequency in Hz (undamped)
-phaseY = 0 # phase offset in radians
-dampingY = 0.01 # damping ratio
+# slider positions for every parameter
+axcolor = 'lightgoldenrodyellow'
+#                      x,    y,   length, height
+axduration = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
+axamplitudex = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+axamplitudey = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+axfrequencyx = plt.axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
+axfrequencyy = plt.axes([0.25, 0.25, 0.65, 0.03], facecolor=axcolor)
+axphasex = plt.axes([0.25, 0.3, 0.65, 0.03], facecolor=axcolor)
+axphasey = plt.axes([0.25, 0.35, 0.65, 0.03], facecolor=axcolor)
+axdampingx = plt.axes([0.25, 0.4, 0.65, 0.03], facecolor=axcolor)
+axdampingy = plt.axes([0.25, 0.45, 0.65, 0.03], facecolor=axcolor)
 
-omegaX = 2 * math.pi * frequencyX
-omegaY = 2 * math.pi * frequencyY
+# sliders for every parameter
+#                       position,   name,       min,  max,   start,        stepsize
+sliderduration = Slider(axduration, "Duration", 1000, 10000, valinit=2000, valstep=1000)
+slideramplitudex = Slider(axamplitudex, "Amplitude X", 1, 10, valinit=1, valstep=1)
+slideramplitudey = Slider(axamplitudey, "Amplitude Y", 1, 10, valinit=1, valstep=1)
+sliderfreuquencyx = Slider(axfrequencyx, "Frequency X", 1.5, 10, valinit=1, valstep=0.01)
+sliderfreuquencyy = Slider(axfrequencyy, "Frequency Y", 1, 10, valinit=1, valstep=0.01)
+sliderphasex = Slider(axphasex, "Phase X", 0, 2 * math.pi, valinit=1, valstep=0.01)
+sliderphasey = Slider(axphasey, "Phase Y", 0, 2 * math.pi, valinit=0, valstep=0.01)
+sliderdampingx = Slider(axdampingx, "Damping X", 0, 1, valinit=0.01, valstep=0.01)
+sliderdampingy = Slider(axdampingy, "Damping Y", 0, 1, valinit=0.01, valstep=0.01)
 
-x = [amplitudeX * math.pow(math.e, -dampingX * omegaX * t) * math.sin(math.sqrt(1 - dampingX**2) * omegaX * t + phaseX) for t in time] # a damped sine wave
-y = [amplitudeY * math.pow(math.e, -dampingY * omegaY * t) * math.sin(math.sqrt(1 - dampingY**2) * omegaY * t + phaseY) for t in time] # a damped sine wave
+def update(val):
+    ax.cla()
 
-# plt.plot(time, x)
-# plt.plot(time, y)
+    duration = int(sliderduration.val)
+    amplitudeX = slideramplitudex.val
+    frequencyX = sliderfreuquencyx.val
+    phaseX = sliderphasex.val
+    dampingX = sliderdampingx.val
+    amplitudeY = slideramplitudey.val
+    frequencyY = sliderfreuquencyy.val
+    phaseY = sliderphasey.val
+    dampingY = sliderdampingy.val
 
-plt.plot(x, y)
+    time = [n*0.01 for n in range(duration)]
+    omegaX = 2 * math.pi * frequencyX
+    omegaY = 2 * math.pi * frequencyY
+
+    x = [amplitudeX * math.pow(math.e, -dampingX * omegaX * t) * math.sin(math.sqrt(1 - dampingX**2) * omegaX * t + phaseX) for t in time] # a damped sine wave
+    y = [amplitudeY * math.pow(math.e, -dampingY * omegaY * t) * math.sin(math.sqrt(1 - dampingY**2) * omegaY * t + phaseY) for t in time] # a damped sine wave
+
+    ax.plot(x, y)
+
+sliderduration.on_changed(update)
+slideramplitudex.on_changed(update)
+slideramplitudey.on_changed(update)
+sliderfreuquencyx.on_changed(update)
+sliderfreuquencyy.on_changed(update)
+sliderphasex.on_changed(update)
+sliderphasey.on_changed(update)
+sliderdampingx.on_changed(update)
+sliderdampingy.on_changed(update)
+
+update(0) # initial drawing
 plt.show()
