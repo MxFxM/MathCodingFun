00be0f3e47ab5a01865d2b1a4278fa8e9c4fdb1f
MxFxM <mxfxmmueller@gmail.com>
2020-06-08 18:15:20 +0200

    added dampened oscillation

--- pendulumShapes/pendulum1.py
+++ pendulumShapes/pendulum1.py
@@ -3,10 +3,15 @@ import math
 
 time = [n*0.01 for n in range(2000)]
 
-frequency = 1 # frequency in Hz
+amplitude = 1 # amplitude of oscillation
+frequency = 1 # frequency in Hz (undamped)
+phase = 0 # phase offset in radians
+damping = 0.1 # damping ratio
+
 omega = 2 * math.pi * frequency
 
-y = [math.sin(omega*t) for t in time] # a simple sine wave
+# y = [math.sin(omega*t) for t in time] # a simple sine wave
+y = [amplitude * math.pow(math.e, -damping * omega * t) * math.sin(math.sqrt(1 - damping**2) * omega * t) for t in time] # a damped sine wave
 
 plt.plot(time, y)
 plt.show()
